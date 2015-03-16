a4a40d8e59e713c7bead7a0a127cf0a871ae8370
Denis Siavichay <dbsiavichay@gmail.com>
2015-03-16 16:59:33 -0500

    Admin del módulo de localización creado.

--- app_localizacion/admin.py
+++ app_localizacion/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class ProvinciaAdmin(admin.ModelAdmin):
+	list_display = ('codigo', 'nombre', 'edit')
+	list_display_links = ('edit',)
+	search_fields = ('nombre',)
+
+class CantonAdmin(admin.ModelAdmin):
+	list_display = ('codigo', 'nombre', 'provincia', 'edit')
+	list_display_links = ('edit',)
+	list_filter = ('provincia',)
+	search_fields = ('nombre',)
+
+class ParroquiaAdmin(admin.ModelAdmin):
+	list_display = ('codigo', 'nombre', 'canton', 'edit')
+	list_display_links = ('edit',)
+	list_filter = ('canton',)
+	search_fields = ('nombre',)
+
+class SectorAdmin(admin.ModelAdmin):	
+	list_display = ('nombre', 'parroquia', 'edit')
+	list_display_links = ('edit',)
+	list_filter = ('parroquia',)
+	search_fields = ('nombre',)
+
+class AsociacionAdmin(admin.ModelAdmin):
+	list_display = ('nombre', 'responsable', 'cordenadax', 'cordenaday', 'sector', 'tecnico', 'edit',)
+	list_display_links = ('edit',)
+	list_filter = ('sector', 'tecnico',)
+	search_fields = ('nombre', 'responsable',)
+
+admin.site.register(Provincia, ProvinciaAdmin)
+admin.site.register(Canton, CantonAdmin)
+admin.site.register(Parroquia, ParroquiaAdmin)
+admin.site.register(Sector, SectorAdmin)
+admin.site.register(Asociacion, AsociacionAdmin)
+
