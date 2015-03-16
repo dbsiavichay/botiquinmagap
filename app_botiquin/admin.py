a726ae42ba1684d9c816c7ae3dbf6afd43a10247
Denis Siavichay <dbsiavichay@gmail.com>
2015-03-16 18:15:31 -0500

    Admin del módulo botiquín creado

--- app_botiquin/admin.py
+++ app_botiquin/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class TipoProductoAdmin(admin.ModelAdmin):
+	list_display = ('nombre', 'edit')
+	list_display_links = ('edit',)
+	search_fields = ('nombre',)
+
+class GrupoProductoAdmin(admin.ModelAdmin):
+	list_display = ('nombre', 'edit')
+	list_display_links = ('edit',)
+	search_fields = ('nombre',)
+
+class MedidaProductoAdmin(admin.ModelAdmin):
+	list_display = ('nombre', 'edit')
+	list_display_links = ('edit',)	
+	search_fields = ('nombre',)
+
+class ProductoAdmin(admin.ModelAdmin):
+	list_display = ('nombre', 'compuesto', 'presentacion', 'precio_referencial', 'registro_sanitario', 'medida', 'edit')	
+	list_display_links = ('edit',)
+	list_filter = ('tipo', 'grupo',)
+	search_fields = ('nombre', 'compuesto', 'presentacion', 'registro_sanitario')
+
+admin.site.register(TipoProducto, TipoProductoAdmin)
+admin.site.register(GrupoProducto, GrupoProductoAdmin)
+admin.site.register(MedidaProducto, MedidaProductoAdmin)
+admin.site.register(Producto, ProductoAdmin)
