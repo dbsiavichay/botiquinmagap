b676a5a36421bf8672bdb741392d4bcbda748909
Denis Siavichay <dbsiavichay@gmail.com>
2015-03-17 15:30:08 -0500

    Instalacion de django-rest-framework en settings

--- botiquin_proj/settings.py
+++ botiquin_proj/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'app_localizacion',
     'app_botiquin',
     'app_ventas',
