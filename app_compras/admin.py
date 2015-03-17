67288c8194adce5bbdec372f11f9334cdf3f8f74
Denis Siavichay <dbsiavichay@gmail.com>
2015-03-16 22:05:04 -0500

    Admid del módulo compras creado

--- app_compras/admin.py
+++ app_compras/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class DetalleCompraInline(admin.TabularInline):
+	model = DetalleCompra
+	extra = 1
+	
+class CompraAdmin(admin.ModelAdmin):
+	list_display = ('asociacion','fecha', 'valor_total', 'edit',)
+	list_display_links = ('edit',)
+	list_filter = ('asociacion',)
+	search_fields = ('asociacion__nombre', 'fecha',)
+	inlines = [DetalleCompraInline,]
+
+
+# class DetalleCompraAdmin(admin.ModelAdmin):
+# 	list_display = ('compra', 'producto', 'cantidad', 'costo_unitario', 'costo_total', 'edit',)
+# 	list_display_links = ('edit',)	
+# 	search_fields = ('producto',)		
+
+
+admin.site.register(Compra, CompraAdmin)
+# admin.site.register(DetalleCompra, DetalleCompraAdmin)
