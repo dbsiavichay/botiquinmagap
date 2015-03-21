b2337fc4ba06a095b4ef44bec1de37e5776b1899
Denis Siavichay <dbsiavichay@gmail.com>
2015-03-21 13:33:43 -0500

    Registro de urls API del módulo Botiquín

--- botiquin_proj/urls.py
+++ botiquin_proj/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from rest_framework import routers
 from app_localizacion.views import *
+from app_botiquin.views import *
 
 router = routers.DefaultRouter()
 
@@ -10,6 +11,11 @@ router.register(r'localizacion/cantones', CantonViewSet)
 router.register(r'localizacion/parroquias', ParroquiaViewSet)
 router.register(r'localizacion/sectores', SectorViewSet)
 router.register(r'localizacion/asociaciones', AsociacionViewSet)
+
+router.register(r'botiquin/tiposproducto', TipoProductoViewSet)
+router.register(r'botiquin/gruposproducto', GrupoProductoViewSet)
+router.register(r'botiquin/medidasproducto', MedidaProductoViewSet)
+router.register(r'botiquin/productos', ProductoViewSet)
 
 urlpatterns = patterns('',
     # Examples:
