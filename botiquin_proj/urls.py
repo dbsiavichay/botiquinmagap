9a0d28abe6ec762934915878d3966100b207a247
Denis Siavichay <dbsiavichay@gmail.com>
2015-03-21 11:48:38 -0500

    Registro de urls del API para el módulo de localización

--- botiquin_proj/urls.py
+++ botiquin_proj/urls.py
@@ -1,10 +1,20 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from rest_framework import routers
+from app_localizacion.views import *
+
+router = routers.DefaultRouter()
+router.register(r'localizacion/asociaciones', AsociacionViewSet)
+router.register(r'localizacion/sectores', SectorViewSet)
+router.register(r'localizacion/parroquias', ParroquiaViewSet)
+router.register(r'localizacion/cantones', CantonViewSet)
+router.register(r'localizacion/provincias', ProvinciaViewSet)
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'botiquin_proj.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^', include(router.urls)),
     url(r'^admin/', include(admin.site.urls)),
 )
