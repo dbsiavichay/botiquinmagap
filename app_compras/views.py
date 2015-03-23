b1f5e0ef6929ff45c728ce59ce2dbcfd7590ac89
Denis Siavichay <dbsiavichay@gmail.com>
2015-03-22 22:25:22 -0500

    Views del módulo Compras

--- app_compras/views.py
+++ app_compras/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import *
+from .models import *
 
-# Create your views here.
+class CompraViewSet(viewsets.ModelViewSet):
+	queryset = Compra.objects.all()
+	serializer_class = CompraSerializer
+
+	def get_queryset(self):
+		queryset = Compra.objects.all()		
+		id_asociacion = self.request.QUERY_PARAMS.get('asociacion', None)	
+		if id_asociacion is not None:
+			queryset = queryset.filter(asociacion__id=id_asociacion)
+		return queryset
+
+class DetalleCompraViewSet(viewsets.ModelViewSet):
+	queryset = DetalleCompra.objects.all()
+	serializer_class = DetalleCompraSerializer
+
+	def get_queryset(self):
+		queryset = DetalleCompra.objects.all()
+		id_compra = self.request.QUERY_PARAMS.get('compra', None)				
+		if id_compra is not None:
+			queryset = queryset.filter(compra__id=id_compra)
+		return queryset
