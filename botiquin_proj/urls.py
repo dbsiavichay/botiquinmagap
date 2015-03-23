b43abe651e299b0038fd1d48d359c1772e6593b9
Denis Siavichay <dbsiavichay@gmail.com>
2015-03-22 22:12:15 -0500

    Registro de urls para el módulo Ventas

--- botiquin_proj/urls.py
+++ botiquin_proj/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from rest_framework import routers
 from app_localizacion.views import *
 from app_botiquin.views import *
+from app_ventas.views import *
 
 router = routers.DefaultRouter()
 
@@ -16,6 +17,13 @@ router.register(r'botiquin/tiposproducto', TipoProductoViewSet)
 router.register(r'botiquin/gruposproducto', GrupoProductoViewSet)
 router.register(r'botiquin/medidasproducto', MedidaProductoViewSet)
 router.register(r'botiquin/productos', ProductoViewSet)
+
+router.register(r'ventas/clientes', ClienteViewSet)
+router.register(r'ventas/enfermedades', EnfermedadViewSet)
+router.register(r'ventas/especies', EspecieViewSet)
+router.register(r'ventas/ventas', VentaViewSet)
+router.register(r'ventas/detallesventa', DetalleVentaViewSet)
+router.register(r'ventas/usosventa', UsoVentaViewSet)
 
 urlpatterns = patterns('',
     # Examples:
