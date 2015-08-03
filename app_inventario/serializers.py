b54c78d5c3b18301705c53b3a8db8bfd8d33fd7a
Denis Siavichay <dbsiavichay@gmail.com>
2015-08-03 17:58:58 -0500

    Dias restantes en la caducidad

--- app_inventario/serializers.py
+++ app_inventario/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import *
+from datetime import date
 
 class InventarioSerializer(serializers.ModelSerializer):
 	valor_total = serializers.SerializerMethodField()
@@ -12,6 +13,13 @@ class InventarioSerializer(serializers.ModelSerializer):
 		return obj.cantidad * obj.valor_unitario
 
 class CaducidadSerializer(serializers.ModelSerializer):
+	dias = serializers.SerializerMethodField()
+
 	class Meta:
 		model = Caducidad
-		fields = ('id', 'fecha', 'cantidad', 'producto', 'asociacion',)
+		fields = ('id', 'fecha', 'cantidad', 'dias', 'producto', 'asociacion',)
+
+	def get_dias(self, obj):
+		hoy = date.today()		
+		delta = obj.fecha - hoy
+		return delta.days
--- app_inventario/views.py
+++ app_inventario/views.py
@@ -10,22 +10,26 @@ class InventarioViewSet(viewsets.ModelViewSet):
 	def get_queryset(self):
 		queryset = Inventario.objects.all()		
 		id_asociacion = self.request.QUERY_PARAMS.get('asociacion',None)
-		id_producto = self.request.QUERY_PARAMS.get('producto',None)		
-		if id_asociacion is not None and id_producto is not None:
+		id_producto = self.request.QUERY_PARAMS.get('producto',None)
+		inicial = self.request.QUERY_PARAMS.get('inicial', None)
+
+		if id_asociacion is not None and inicial is not None:
+			queryset = queryset.filter(asociacion__id=id_asociacion, es_inicial=True)
+		elif id_asociacion is not None and id_producto is not None:
 			queryset = queryset.filter(asociacion__id=id_asociacion, producto__id=id_producto)				
 		elif id_asociacion is not None:
 			queryset = queryset.filter(asociacion__id=id_asociacion)
 		return queryset	
 
-class CaducidadViewSet(viewsets.ModelViewSet):
+class CaducidadViewSet(viewsets.ModelViewSet):	
 	queryset = Caducidad.objects.all()
 	serializer_class = CaducidadSerializer
 
 	def get_queryset(self):
 		queryset = Caducidad.objects.all()
 		id_asociacion = self.request.QUERY_PARAMS.get('asociacion',None)
-		id_producto = self.request.QUERY_PARAMS.get('producto',None)
-				
+		id_producto = self.request.QUERY_PARAMS.get('producto',None)				
+
 		if id_asociacion is not None and id_producto is not None:
 			queryset = queryset.filter(asociacion__id=id_asociacion, producto__id=id_producto)
 		elif id_asociacion is not None:
